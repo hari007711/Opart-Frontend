a10082ada657ade05f9e4c29d8d54e9c615d785b
hari007711 <hari0077kumar@gmail.com>
2025-10-10 11:20:00 +0530

    url change

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "https://main.ddax7lra1g7kw.amplifyapp.com/",
+  baseURL: "https://opart-frontend.vercel.app",
 });
